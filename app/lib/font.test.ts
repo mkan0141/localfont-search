2fccc34e182c3a7dc41ec19ad15aa1a7d06e45e6
mkan0141 <kanzume0141@gmail.com>
2023-06-17 13:42:25 +0900

    main chore(package): add getLocalFontList function

--- app/lib/font.test.ts
+++ app/lib/font.test.ts
@@ -1,6 +1,6 @@
 import { vi, describe, it, expect, afterEach } from 'vitest';
 
-import { isSupportedQueryLocalFontsApi } from './font';
+import { isSupportedQueryLocalFontsApi, getLocalFontList } from './font';
 
 describe('isSupportedQueryLocalFontsApi', () => {
   afterEach(async () => {
@@ -14,5 +14,54 @@ describe('isSupportedQueryLocalFontsApi', () => {
 
   it('not supported queryLocalFonts API', () => {
     expect(isSupportedQueryLocalFontsApi()).toBe(false);
+  });
+});
+
+describe('getLocalFontList', () => {
+  afterEach(async () => {
+    await delete window['queryLocalFonts'];
+  });
+
+  it('not supported queryLocalFonts API', async () => {
+    const result = await getLocalFontList();
+    expect(result).toEqual([]);
+  });
+
+  it('supported queryLocalFonts API', async () => {
+    window.queryLocalFonts = vi.fn(async () => {
+      return Promise.resolve([
+        {
+          family: 'Noto Sans JP',
+          fullName: 'Noto Sans JP Bold',
+          postscriptName: 'NotoSansJP-Bold',
+          style: 'Bold',
+        },
+        {
+          family: 'Noto Sans JP',
+          fullName: 'Noto Sans JP Italic',
+          postscriptName: 'NotoSansJP-Italic',
+          style: 'Italic',
+        },
+        {
+          family: 'Jost',
+          fullName: 'Jost Medium Italic',
+          postscriptName: 'Jost-MediumItalic',
+          style: 'Medium Italic',
+        },
+      ]);
+    });
+
+    expect(await getLocalFontList()).toEqual([
+      {
+        family: 'Noto Sans JP',
+        isBold: true,
+        isItalic: true,
+      },
+      {
+        family: 'Jost',
+        isBold: false,
+        isItalic: false,
+      },
+    ]);
   });
 });
--- app/lib/font.ts
+++ app/lib/font.ts
@@ -1,5 +1,42 @@
+import { groupBy } from 'lodash-es';
+
+type FontData = {
+  family: string;
+  fullName: string;
+  postscriptName: string;
+  style: string;
+};
+
+type Font = {
+  family: string;
+  isBold: boolean;
+  isItalic: boolean;
+};
+
+declare global {
+  interface Window {
+    queryLocalFonts?: () => Promise<FontData[]>;
+  }
+}
+
 const isSupportedQueryLocalFontsApi = () => {
   return typeof window === 'object' && 'queryLocalFonts' in window;
 };
 
-export { isSupportedQueryLocalFontsApi };
+const getLocalFontList = async (): Promise<Font[]> => {
+  if (!isSupportedQueryLocalFontsApi()) return [];
+
+  const localFontList = await window.queryLocalFonts!();
+
+  return Object.values(groupBy(localFontList, (font) => font.family)).map(
+    (fonts: FontData[]) => {
+      return {
+        family: fonts[0].family,
+        isBold: fonts.some((font) => font.style === 'Bold'),
+        isItalic: fonts.some((font) => font.style === 'Italic'),
+      };
+    }
+  );
+};
+
+export { isSupportedQueryLocalFontsApi, getLocalFontList };
