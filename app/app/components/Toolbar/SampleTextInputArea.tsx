3e6a061922e0490a9c4f8db514a9ca22cbc49804
mkan0141 <kanzume0141@gmail.com>
2023-06-19 00:31:51 +0900

    main chore(SampleTextInputArea): add SampleTextInputArea component

---
+++ app/app/components/Toolbar/SampleTextInputArea.tsx
@@ -0,0 +1,47 @@
+import { ChangeEvent } from 'react';
+import { useAtom } from 'jotai';
+
+import { Cross1Icon } from '@radix-ui/react-icons';
+import { sampleTextAtom } from '@/common/atom/index';
+
+type Props = {
+  className?: string;
+};
+
+const SampleInputArea = ({ className }: Props) => {
+  const [sampleText, setSampleText] = useAtom(sampleTextAtom);
+
+  const onChangeSampleText = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setSampleText(e.target.value);
+  };
+
+  const onClearSampleText = () => setSampleText('');
+
+  return (
+    <div
+      className={`${className} relative rounded-md border border-slate-300 px-2`}
+    >
+      <input
+        className="h-full text-sm outline-none"
+        onChange={onChangeSampleText}
+        value={sampleText}
+        placeholder="サンプルテキストを入力する"
+        autoFocus={false}
+        autoComplete="off"
+        autoCapitalize="off"
+        spellCheck="false"
+      />
+      {sampleText && (
+        <button
+          className="absolute right-1 top-1/2 -translate-y-1/2"
+          onClick={onClearSampleText}
+        >
+          <Cross1Icon />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export { SampleInputArea };
